cb78852097d63fe46315b9362acb8891adbcebdc
Shimakzu <150023880+Shimakzu@users.noreply.github.com>
2024-02-24 03:47:29 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This Site is Profile Site By Momoyama Tech Club
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="header">
+        <div class="header-logo">
+          <img src="https://global.toyota/pages/global_toyota/mobility/toyota-brand/emblem_ogp_001.png"/>
+          
+        </div>
+        <div class="header-list">
+          <h1>2JZ-GTE VVT-i</h1>
+        </div>
+        <div class="contents-item">
+        <img src="https://lh3.googleusercontent.com/NRnsn4grQgJLF1bzl3UNiNjrxUDYWbgOGjzmp8imHr7uJMmMHqBL6zyxiAAlBN_t1BkSC9ZyaTs2lFA5hK09mJkfh3QJIoVaeHA8G0fa=s750"/>
+        </div>
       </header>
+      <main class="main">
+        <b className="engine-type">直列6気筒DOHC24バルブエンジンツインターボ</b>
+        <p className="explanation">2JZ-GTE VVT-iはJZエンジンの一型式であり、総排気量は2,997cc、
+        バルブ数は24バルブで、二基のターボチャージャーで過給される。<br/>
+        JZ系エンジンの中で最強のモデルとなる2JZ-GEのターボ仕様のエンジンである。<b/>
+        国内外からは「トヨタ最強エンジン」などと評されることがある。<br/>
+        特にドリフトでは現在でもベース車両のメーカー問わず使用されており、<br/>
+        FIAの国際大会でも約半数が2JZ搭載車であった。<br/>
+        カタログ上では1JZ-GTEに引き続き280psの最高出力であるが、<br/>
+        日産・RB26DETTと並んで1000ps級のチューニングを施される例が確認できる。<br/>
+        1JZ-GTEにヤマハ発動機が関与している影響でこのエンジンにもヤマハ発動機が関与しているという論調が存在するが、ヤマハ発動機は当該エンジンには一切関与していない。</p>
+
+      </main>
+      <footer class="footer">
+      </footer>
+
     </div>
   );
 }
